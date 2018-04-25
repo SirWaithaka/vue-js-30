79cf2f43e488859e43a5374429c319db1577cdad
craftsmon <craftsmon@outlook.com>
2018-04-25 23:59:04 +0300

    Home page

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="h1 my-5 text-center">Course from Javascript in 30 days</h1>
+    <div class="container">
+      <div class="row">
+        <b-card  class="col col-md-6" bg-variant="dark" text-variant="white" title="Javascript Drum Kit">
+          <p class="card-text">
+            Build a drum kit and play it using keyboard keys
+          </p>
+          <b-button :click="navigateTo({ name: 'DrumMachine' })" href="#" variant="primary">View</b-button>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
